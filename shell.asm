1f78de0c3ff237375a8570eddb00df8a2ac63e01
InSaNiTyCtEaTuReS <void7535@gmail.com>
2026-03-18 11:15:22 -0500

    Update shell

--- shell.asm
+++ shell.asm
@@ -1,3 +1,15 @@
+;header
+brai 6
+{3};tag count
+{1};tag len 1
+@os  ;tag 1
+{1};tag len 2
+@.exe;tag 2
+{3};tag len 3
+@immutable   ;tag 3
+;end of file header
+
+
 #CONST byte = r0
 #CONST color = r1
 #CONST index = r2
@@ -8,15 +20,15 @@ addi index,32,index
 <startlabel
 jmp
 
-;32 bytes offset from start of file
-@______       ___      ______   __   __                          
-@|  _  \     / _ \     |  _  |  | | / /  (%%%%%%) (%%%%%%) /%%%%)
-@| | |  |   / / \ \    | |_| |  | |/ /     |%%|     |%%|   \%\   
-@| | |  |  / /___\ \   | __  |  |   \      |%%|     |%%|    %%%%)
-@| |_|  | / /     \ \  | | \ \  | |\ \     |%%|     |%%|   /%/   
-@|_____/ /_/       \_\ |_|  \_\ |_| \_\  (%%%%%%)   |%%|   \%%%%)
-@----------------------------------------------------------------
-@start typing----meow                                            
+>code
+@______       ___      ______   __   __                          ;
+@|  _  \     / _ \     |  _  |  | | / /  (%%%%%%) (%%%%%%) /%%%%);
+@| | |  |   / / \ \    | |_| |  | |/ /     |%%|     |%%|   \%\   ;
+@| | |  |  / /___\ \   | __  |  |   \      |%%|     |%%|    %%%%);
+@| |_|  | / /     \ \  | | \ \  | |\ \     |%%|     |%%|   /%/   ;
+@|_____/ /_/       \_\ |_|  \_\ |_| \_\  (%%%%%%)   |%%|   \%%%%);
+@----------------------------------------------------------------;
+@start typing----meow                                            ;
 ;544 byte offset
 ;alpha (higher = more transparent
 {0}
@@ -48,6 +60,7 @@ lp r4
 load r3
 or r3,r1,r1
 addi r4,1,r4
+<color_code
 
 ;loop to display all 512 bytes of ascii data
     lp index

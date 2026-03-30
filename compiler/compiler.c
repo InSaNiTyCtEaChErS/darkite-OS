438d6e339eb3b4570522a5a6bcf865e8f2cc5281
InSaNiTyCtEaTuReS <void7535@gmail.com>
2026-03-30 14:36:40 -0500

    Update compiler

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -13,57 +13,119 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+#define FILE_TO_LOAD "/working_file.txt"
+#define FILE_TO_WRITE "/runtime.txt"
 
-int compile_check(u8 *pointer, u32 length){
-    char start = *pointer;
-    char next = *(pointer+1);
+int getreg(u8 *pointer){
+    bool reg = false;
+    bool quit = false;
+    int regnum = 0;
+    for (u8 i; i < 255; i++){
+        if (*(pointer+i) == 'r'){
+            reg = true;
+            continue;
+        }
+        regnum = regnum*10;
+        switch (*(pointer+i)) // translate characters into numbers
+        {
+            case '0': break;// too lazy to use strtol apparently
+            case '1':regnum += 1;break;
+            case '2':regnum += 2;break;
+            case '3':regnum += 3;break;
+            case '4':regnum += 4;break;
+            case '5':regnum += 5;break;
+            case '6':regnum += 6;break;
+            case '7':regnum += 7;break;
+            case '8':regnum += 8;break;
+            case '9':regnum += 9;break;
+            case ';':quit = true;break;
+            case ',':quit = true;break;
+            case '\0':quit = true;break;
+            default:
 
-    switch (char){
-
-        case 'a':
-            if (next == 'w'){
-
-            }else{
-                
-            }
-        case 's':
-            if (next == 'w'){
-                
-            }else{
-
-            }
-        case 'b':
-            switch (next){
-                case 'e': // branch equal
-
-                
-                case 'n': // branch not equal
- 
-                
-                case 'a': // branch above equal
- 
-                
-                case 'b': // branch below
- 
-                
-                case 'l': // branch less 
- 
-                
-                case 'g': // branch greater equal
- 
-
-                case 'r': // branch always
-
-                
-            }
-            
+        }
+        if (quit == true){
+            break;
+        }
     }
+    regnum = regnum / 10;
 
+    return regnum;
+}
+
+int compile_check_line(u8 *pointer, u32 length){ // compile a line into data-------------------------------------------------------------------------------------------------------
+    //find opcode
+    char opcodes[32][3] = {"add","awc","sub","swc","nan","and","lsl","lsr","jum","bra","be ","bne","bl ","bge","bae","bb ","loa","sto","cmp","cal","ret","int","lui","unu"};
+    bool inst = true;
+    u8 opcode = -1;
+    for (int i; i<32;i++){
+        inst = true;
+        if (*(pointer) != opcodes[i][0]){
+            inst = false;
+        }
+        if (*(pointer+1) != opcodes[i][1]){
+            inst = false;
+        }
+        if (*(pointer+2) != opcodes[i][2]){
+            inst = false;
+        }
+        if (inst == true){
+            opcode = i;
+            break;
+        }
+    }
+    if (inst == false){
+        return -1; // invalid opcode
+    }
+    
+    // find space
+    int space = -1;
+    bool end = false;
+    for (int i; i<length;i++){
+        if (*(pointer+i) == ' '){
+            space = i;
+        }
+        if (*(pointer+i) == '\n'){
+            space = i;
+            end = true;
+        }
+    }
+    if (end == true){
+        //encode zero argument instructions
+        return(opcode<<16);
+    }
 
 }   
 
-int main(){
-    char str[] = "jmp";
-    char *string = str;
-    compile_check(string,sizeof(string));
+int compile(u8 *pointer, u32 length){ // compile a pointer and length into instruction code-----------------------------------------------------------------------------------
+    char line[64] = {};
+    u8 index = 0;
+
+    for (int i; i<length; i++){
+        if (*(pointer+i) == '\n'){
+            //pad out line with spaces
+            for (int j = index; j<64;j++){
+                line[j] = ' ';
+            }
+            //compile line
+            if (line[0] == ';'){
+                continue;
+            }
+
+
+        }else{
+            line[index] = *(pointer+i);
+            index ++;
+        }
+    }
+
+
+    
+
+    return 0;
+}
+
+
+int main(void){
+    
 }
